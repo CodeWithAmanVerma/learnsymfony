5a27d54e882863384d289fdd0bb66b3f8d74342c
Aman Verma <aman@commercepundit.com>
2021-01-08 18:57:33 +0530

    Create Post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +12,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        dump('aasda');
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +15,37 @@ class PostController extends AbstractController
     /**
      * @Route("/post", name="post")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/post/create", name="create_post")
+     */
+
+    public function create(Request $request){
+        $post = new Post();
+        
+        $postForm = $this->createForm(PostType::class, $post);
+        $postForm->handleRequest($request);
+
+        if($postForm->isSubmitted() && $postForm->isValid()){
+            $post->setPostStatus('active');
+            $post->setPostAuthor($this->getUser());
+            $post->setCreated(new \DateTime());
+            $post->setUpdated(new \DateTime());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post); // query prepared
+            $entityManager->flush(); // execute query
+
+            return $this->redirectToRoute('post');
+        }
+
+        return $this->render('post/create.html.twig', [
+            'postForm' => $postForm->createView()
         ]);
     }
 }
